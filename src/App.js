05921eb92aa243256a8017d34308bc429fccc21c
DavidPsyko <davidjohnsonny@gmail.com>
2022-05-21 22:16:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+// import FooterPix from './content/Footer.jpg';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div class='main'>
+    <div class='FrontPage'>sup fukcer</div>
+    <div class='StickyNav'>StickyNav</div> 
+    <div class='SecondPage'>dfd
+        <div class='Carousel'>carousal</div>
     </div>
+    <div class='ThirdPage'>3rdpage</div>
+    <div class='Footer'>footer </div>
+  </div>
   );
 }
 
