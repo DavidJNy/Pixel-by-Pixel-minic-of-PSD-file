5d23ce400fd9f511dbb940a7eabfc8cb81376e40
DavidPsyko <davidjohnsonny@gmail.com>
2022-05-25 21:51:08 -0700

    Added all PSD files and static layout.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import './App.css';
-// import FooterPix from './content/Footer.jpg';
+import frontPage from './content/FrontPage.jpg';
+import activeLink from './content/activeLink.jpg';
+import StickyNav from './content/StickyNav.jpg';
+import SecondPage from './content/SecondPage.jpg';
+import LastPage from './content/LastPage.jpg';
+import FooterPix from './content/Footer.jpg';
+
 
 function App() {
 
   return (
-  <div class='main'>
-    <div class='FrontPage'>sup fukcer</div>
-    <div class='StickyNav'>StickyNav</div> 
-    <div class='SecondPage'>dfd
-        <div class='Carousel'>carousal</div>
+  <div class='container'>
+    <div class='firstPage'>
+        <img src={frontPage} alt='' class='frontPage' ></img>
+        <div class='topNav'>
+          <img src={activeLink} alt='' class='activeLink1' ></img>
+          <img src={activeLink} alt='' class='activeLink2' ></img>
+        </div>
     </div>
-    <div class='ThirdPage'>3rdpage</div>
-    <div class='Footer'>footer </div>
+    <div class='StickyNav'>
+        <img src={StickyNav} alt='' ></img>
+    </div> 
+    <div class='SecondPage'>
+        <img src={SecondPage} alt='' ></img>
+        <div class='Carousel'>
+        </div>
+    </div>
+      <div class='LastPage'>
+        <img src={LastPage} alt='' ></img>
+    </div>
+    <div class='Footer'>
+        <img src={FooterPix} alt='' ></img>
+    </div>
   </div>
   );
 }
