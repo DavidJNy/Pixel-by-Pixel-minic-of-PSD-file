3a13504dfb1f4f6a98d5e4ade4a0b76a85e5c3c9
David Ny <davidjohnsonny@gmail.com>
2022-05-29 20:16:18 -0700

    Adjusting topnav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import frontPage from './content/FrontPage.jpg';
+import topNav from './content/TopNav.jpg';
 import activeLink from './content/activeLink.jpg';
 import StickyNav from './content/StickyNav.jpg';
 import SecondPage from './content/SecondPage.jpg';
@@ -10,28 +11,36 @@ import FooterPix from './content/Footer.jpg';
 function App() {
 
   return (
-  <div class='container'>
-    <div class='firstPage'>
-        <img src={frontPage} alt='' class='frontPage' ></img>
+    <div class='container'>
+      <div class='firstPage'>
+        <img src={frontPage} alt='' class='' ></img>
         <div class='topNav'>
-          <img src={activeLink} alt='' class='activeLink1' ></img>
-          <img src={activeLink} alt='' class='activeLink2' ></img>
+          <a href='/first' class='history1'><div>
+          </div>
+            </a>
+          <a href='/second' class='team2'><div>
+          </div>
+          </a>
         </div>
-    </div>
-    <div class='StickyNav'>
+      </div>
+
+    {/* <div class='StickyNav'>
         <img src={StickyNav} alt='' ></img>
     </div> 
+
     <div class='SecondPage'>
         <img src={SecondPage} alt='' ></img>
         <div class='Carousel'>
         </div>
     </div>
-      <div class='LastPage'>
-        <img src={LastPage} alt='' ></img>
+
+    <div class='LastPage'>
+      <img src={LastPage} alt='' ></img>
     </div>
+
     <div class='Footer'>
         <img src={FooterPix} alt='' ></img>
-    </div>
+    </div> */}
   </div>
   );
 }
